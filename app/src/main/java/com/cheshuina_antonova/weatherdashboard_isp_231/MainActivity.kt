7406492aef2fc3ee84ba84de47a47e60110374b7
s0m1k111 <spavel1207@gmail.com>
2026-04-01 14:57:20 +0300

    Step 10: Add loading progress indicator

--- app/src/main/java/com/cheshuina_antonova/weatherdashboard_isp_231/MainActivity.kt
+++ app/src/main/java/com/cheshuina_antonova/weatherdashboard_isp_231/MainActivity.kt
@@ -98,6 +98,14 @@ fun WeatherDashboardScreen(
                 style = MaterialTheme.typography.bodyMedium
             )
         }
+        if (weatherState.loadingProgress.isNotEmpty()){
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = weatherState.loadingProgress,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        }
     }
 }
 @Composable
