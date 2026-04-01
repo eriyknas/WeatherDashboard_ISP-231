2397c64b10ffbda78fdfffdc062fe6a18017e5ee
s0m1k111 <spavel1207@gmail.com>
2026-04-01 14:46:00 +0300

    Step 8: Implement parallel loading with async

--- app/src/main/java/com/cheshuina_antonova/weatherdashboard_isp_231/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/cheshuina_antonova/weatherdashboard_isp_231/viewmodel/WeatherViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cheshuina_antonova.weatherdashboard_isp_231.data.WeatherData
 import com.cheshuina_antonova.weatherdashboard_isp_231.data.WeatherRepository
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -23,13 +24,20 @@ class WeatherViewModel : ViewModel(){
                 error = null
             )
             try {
-                val temperature = repository.fetchTemperature()
-                _weatherState.value = _weatherState.value.copy(temperature = temperature)
-                val humidity = repository.fetchHumidity()
-                _weatherState.value = _weatherState.value.copy(humidity = humidity)
-                val windSpeed = repository.fetchWindSpeed()
-                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
-                _weatherState.value = _weatherState.value.copy(isLoading = false)
+                val temperatureDeferred = async { repository.fetchTemperature() }
+                val humidityDeferred = async { repository.fetchHumidity() }
+                val windSpeedDeferred = async { repository.fetchWindSpeed() }
+                val temperature = temperatureDeferred.await()
+                val humidity = humidityDeferred.await()
+                val windSpeed = windSpeedDeferred.await()
+                _weatherState.value = WeatherData(
+                    temperature = temperature,
+                    humidity = humidity,
+                    windSpeed = windSpeed,
+                    isLoading = false,
+                    error = null
+                )
+
             } catch (e: Exception){
                 _weatherState.value = _weatherState.value.copy(
                     isLoading = false,
